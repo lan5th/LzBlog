8d68a23ab299b756c47bc472d34d453b7cc6986d
lan5th <576247105@qq.com>
2022-08-31 11:57:21 +0800

    更新sql

--- sql/lz_blog.sql
+++ sql/lz_blog.sql
@@ -13,7 +13,8 @@ create table blogs
 );
 
 create index create_time_key
-    on blogs (create_time);
+    on blogs (create_time)
+    comment '时间排序';
 
 create index tag_id_index
     on blogs (tag_id);
@@ -26,7 +27,8 @@ create table comment
     content     varchar(150)                         null,
     create_time timestamp  default CURRENT_TIMESTAMP not null,
     deleted     tinyint(1) default 0                 not null,
-    user_id     bigint                               not null
+    user_id     bigint                               not null,
+    reply_to    bigint                               null comment '回复某个评论的id'
 );
 
 create index blog_id_index
@@ -34,6 +36,44 @@ create index blog_id_index
 
 create index create_time_index
     on comment (create_time);
+
+create table config_info
+(
+    id           bigint auto_increment comment 'id'
+        primary key,
+    data_id      varchar(255)                           not null comment 'data_id',
+    group_id     varchar(255)                           null,
+    content      longtext                               not null comment 'content',
+    md5          varchar(32)                            null comment 'md5',
+    gmt_create   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    gmt_modified datetime     default CURRENT_TIMESTAMP not null comment '修改时间',
+    src_user     text                                   null comment 'source user',
+    src_ip       varchar(50)                            null comment 'source ip',
+    app_name     varchar(128)                           null,
+    tenant_id    varchar(128) default ''                null comment '租户字段',
+    c_desc       varchar(256)                           null,
+    c_use        varchar(64)                            null,
+    effect       varchar(64)                            null,
+    type         varchar(64)                            null,
+    c_schema     text                                   null,
+    constraint uk_configinfo_datagrouptenant
+        unique (data_id, group_id, tenant_id)
+)
+    comment 'config_info' collate = utf8_bin;
+
+create table links
+(
+    id          bigint                               not null
+        primary key,
+    link_name   varchar(50)                          not null,
+    url         varchar(200)                         not null,
+    create_time timestamp  default CURRENT_TIMESTAMP not null,
+    type        int                                  null comment '推荐文章：1
+友情链接：2
+',
+    enabled     tinyint(1) default 1                 null
+)
+    comment '链接';
 
 create table tags
 (
@@ -57,13 +97,12 @@ create table user_info
     id          bigint                               not null
         primary key,
     name        varchar(30)                          not null,
-    qq_id       bigint                               null,
-    avatar_url  varchar(50)                          null,
+    avatar_url  varchar(200)                         null,
     deleted     tinyint(1) default 0                 not null,
     is_admin    tinyint(1) default 0                 not null,
-    create_time timestamp  default CURRENT_TIMESTAMP not null
+    create_time timestamp  default CURRENT_TIMESTAMP not null,
+    openid      varchar(50)                          null comment 'qq登录获取到的openid'
 );
 
-create index qq_id__index
-    on user_info (qq_id);
-
+create index openid__index
+    on user_info (openid);
